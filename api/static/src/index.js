857807752503df39a2362f8cdbe85889fd4add4c
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2024-04-15 15:20:38 +0800

    feat: finalize app

--- api/static/src/index.js
+++ api/static/src/index.js
@@ -1,21 +1,14 @@
+const devUrl = 'http://localhost:5000/secrets';
+const productionUrl = 'https://secrets-generator.vercel.app/secrets';
+
 const generateBtn = document.getElementById('generate-btn');
 const input = document.querySelector('input[type="text"]');
-
-const productionUrl = 'https://secrets-generator.vercel.app/secrets';
-const devUrl = 'http://localhost:5000/secrets';
 
 generateBtn.addEventListener('click', async () => {
   generateBtn.disabled = true;
 
   try {
     const response = await fetch(productionUrl);
-
-    if (!response.ok) {
-      if (response.status === 404) {
-        throw new Error('Resource not found.');
-      }
-      throw new Error(`Request failed. ${response.status}`);
-    }
     const data = await response.json();
     input.value = data;
   } catch (error) {
