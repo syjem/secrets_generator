2010776fba2e17e00920b818547b66616f597f68
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2024-04-16 08:44:08 +0800

    optimized

--- api/app.py
+++ api/app.py
@@ -6,21 +6,22 @@ app = Flask(__name__)
 CORS(app)
 
 
+def secret_keys(char):
+    secret_key = ''.join(secrets.choice(
+        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*(-_=+)') for _ in range(char))
+
+    return secret_key
+
+
 @app.route("/")
 def index():
-
-    secret_key = ''.join(secrets.choice(
-        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*(-_=+)') for _ in range(32))
-
+    secret_key = secret_keys(32)
     return render_template('index.html', secret_key=secret_key)
 
 
 @app.route("/secrets")
 def secret_generators():
-
-    secret_key = ''.join(secrets.choice(
-        'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*(-_=+)') for _ in range(32))
-
+    secret_key = secret_keys(32)
     return jsonify(secret_key)
 
 
