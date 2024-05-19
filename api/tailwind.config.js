079c33503a082489d29575b2985b495d7705540e
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2024-05-19 19:50:23 +0800

    chore: set node modules inside api

--- api/tailwind.config.js
+++ api/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['api/templates/**/*.html', 'api/static/src/**/*.js'],
+  content: ['./templates/**/*.html', './static/src/**/*.js'],
   theme: {
     extend: {},
   },
